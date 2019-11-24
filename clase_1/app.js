769f1d5a2e7a1baa478c250f93b838a62a558c9f
Omar Montalvo <xhamir.one@gmail.com>
2019-11-24 11:50:36 -0600

    Ejemplos

--- clase_1/app.js
+++ clase_1/app.js
@@ -1,3 +1,43 @@
+// Objeto Math
+// var val = Math.PI;
+// val = Math.round(2.6);
+// val = Math.ceil(2.2);
+// val = Math.floor(Math.random()*20+1);
+// console.log(val);
+
+// Operaciones
+// var x = 100;
+// var y = 50;
+//
+// var resultado;
+//
+// resultado = [x+y,
+// x-y,
+// x*y,
+// x/y,
+// x%y,]
+// resultado = x+y;
+// resultado = x-y;
+// resultado = x*y;
+// resultado = x/y;
+// resultado = x%y;
+
+// console.log("El resultado es:", resultado);
+// console.log(resultado);
+
+
+// Conversion de datos
+// var val = "Test";
+// val = "100.30";
+// // val = parseInt(val);
+// val = parseFloat(val);
+// console.log(val);
+// console.log(typeof val);
+// var test = 5;
+// test = String(test);
+// var name = "Omar";
+// console.log(name+test);
+
 // // Referencias-Objetos
 // // Array
 // var hobbies = ["movies", "futbol", "bicicleta"];
