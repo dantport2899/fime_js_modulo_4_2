ac6d3457d2dfd3ad5cd81651474d7bbed282cf97
Omar Montalvo <xhamir.one@gmail.com>
2019-11-24 12:50:12 -0600

    Ejemplos

--- clase_1/app.js
+++ clase_1/app.js
@@ -1,3 +1,135 @@
+// Functions
+function saludo(name= "Omar", apellido="Montalvo") {
+  return 'Hello '+name+' '+apellido;
+}
+
+
+console.log(saludo());
+
+// switch
+
+// var color = 'blue';
+//
+// switch (color) {
+//   case 'yellow':
+//     console.log('Color is yellow');
+//     break;
+//   case 'blue':
+//     console.log('Color is blue');
+//     break;
+//   case 'red':
+//     console.log('Color is red');
+//     break;
+//   default:
+//
+// }
+
+// Comparasions
+// var name = "Omar";
+// var age = 66;
+// var job = "Dev";
+
+// if (age < 15 || age > 65) {
+//   console.log("Entro en esta opción");
+// }
+
+// Rangos
+// if (age > 0 && age < 15) {
+//   console.log("Es menor a 15 o joven");
+// } else if (age >= 15 && age <= 19) {
+//   console.log("Segunda opcion");
+// } else {
+//   console.log("Persona adulta");
+// }
+
+
+// if (job != "Dev") {
+//   console.log("Si es diferente de Test");
+// } else if(job === "Dev"){
+//   console.log('Es igual a dev');
+// } else {
+//   console.log('Diferente de test');
+// }
+
+// if (age > 29) {
+//   console.log("Si es menor o igual a 30");
+// }
+
+// // Si es igual
+// if (age == 29) {
+//   console.log("Si es 29");
+// // SI NO CUMPLE
+// } else {
+//   console.log("No son iguales");
+// }
+
+// DIFERENTE de
+// if (job != "Test") {
+//   console.log("Si es diferente de Test");
+// }
+
+
+// Concatenar
+// var name = "Omar";
+// var age = 29;
+// var job = "Dev";
+// var city = "MTY";
+//
+// var html = '<ul>' +
+//        '<li>Name: ' + name + '</li>' +
+//        '<li>Age: ' + age + '</li>' +
+//        '<li>Job: ' + job + '</li>' +
+//        '<li>City: ' + city + '</li>' +
+//        '</ul>';
+//
+// document.write(html);
+// Validar si mi string contiene cierto string
+// var info = "Omar, 28, test, 2";
+// var test = info.includes('hola');
+// obtienes un booleano
+// console.log(test);
+
+// // Convertir string en arreglo
+// var info = "Omar, 28, test, 2";
+// // Dependiendo el caracter a separar
+// var arreglo = info.split(',');
+// console.log(info);
+// console.log(arreglo);
+
+// var saludo = "Hola";
+// var name = "Omar";
+// var materia = "Javascript";
+
+// var concatenado = saludo+" "+name+" "+"la materia es: "+materia;
+
+// var concatenado = 'Esta es la materia de "Javascript"'
+
+// Longitud de mi String
+// var test = saludo.length;
+
+// Cambiar mayusculas
+// saludo.toUpperCase();
+
+// Cambiar minisculas
+// saludo.toLowerCase();
+
+// Posicion de caracteres
+// saludo[2]
+
+// Obtener el indice de un caracter
+// console.log(saludo[saludo.indexOf('a')]);
+// console.log(saludo.indexOf('a'));
+
+
+
+
+
+
+
+
+
+
+
 // Objeto Math
 // var val = Math.PI;
 // val = Math.round(2.6);
