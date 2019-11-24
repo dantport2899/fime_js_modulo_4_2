8facf631e9729b0d1d17506aeaaf77a8bb487a3a
Omar Montalvo <xhamir.one@gmail.com>
2019-11-24 10:37:06 -0600

    Example tipos de datos

--- clase_1/app.js
+++ clase_1/app.js
@@ -1,1 +1,46 @@
-console.log('Hola Mundo');
+// // Referencias-Objetos
+// // Array
+// var hobbies = ["movies", "futbol", "bicicleta"];
+// // Objetos
+// var address = {
+//   city: "Monterrey",
+//   state: "Nuevo Leon",
+//   info: {
+//     name: "Omar",
+//     age: 29,
+//   },
+//   arreglo: ["movies", "futbol", "bicicleta"]
+// };
+//
+// console.log(address);
+
+// console.log(hobbies[1]);
+
+// // Primitivas
+//
+// // String
+// var name = "Omar Jair";
+//
+// // Number
+// var age = 29;
+//
+// // Boolean
+// var test = true;
+//
+// // Null
+// var car = null;
+//
+// // No definidas
+// var test2;
+//
+// test2 = car;
+// test2 = name;
+
+
+// console.log(name);
+// console.log(age);
+// console.log(test);
+// console.log(car);
+// console.log(test2);
+
+// console.log('Hola Mundo');
