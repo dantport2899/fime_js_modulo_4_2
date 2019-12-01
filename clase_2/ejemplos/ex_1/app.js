f57961afb1452bf0d1677739dd4de1737f5d9322
Omar Montalvo <xhamir.one@gmail.com>
2019-12-01 12:33:12 -0600

    Ejemplos

--- clase_2/ejemplos/ex_1/app.js
+++ clase_2/ejemplos/ex_1/app.js
@@ -1,3 +1,54 @@
+// Document
+var val;
+
+
+// val = document.querySelectorAll('li:nth-child(odd)')
+// val = document.querySelectorAll('p');
+//
+// for (var i = 0; i < val.length; i++) {
+//   val[i].textContent = 'Cambia el contenido'
+// }
+
+// document.querySelector('li').style.color = 'blue';
+//
+// document.querySelector('li:last-child').style.padding = '20px'
+//
+//
+// document.querySelector('li:nth-child(2)').style.color = 'red';
+//
+// document.querySelector('li:nth-child(odd)').style.color = 'yellow';
+// document.querySelector('li:nth-child(even)').style.color = 'red';
+
+
+
+// Obtener elementos por medio de tag
+// val = document.querySelector('p');
+
+// busqueda por clase
+// val = document.querySelector('.titulo');
+// // Busqueda por id
+// val = document.querySelector('#titulo');
+
+console.log(val);
+
+
+
+// Traer un solo elemento por medio de un id
+// val = document.getElementById('titulo');
+// Cambiar solo texto
+// val.textContent = 'Hola mundo';
+// val.innerText = 'Hola Test';
+// Cambiar texto con contenido html
+// val.innerHTML = '<b>Hola</b> Mundo';
+
+// console.log(val);
+
+// Primer vistaso a document
+// val = document.body;
+// val.style.background = 'yellow';
+//
+// val.style.color = "red"
+// console.log(val);
 
 
 // Ejemplo arreglos con objetos
