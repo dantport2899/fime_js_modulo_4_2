3801adc1b4a21cedbfdebdf308eb9545fe8ca4ea
Omar Montalvo <xhamir.one@gmail.com>
2019-12-01 11:25:36 -0600

    Ejemplos

---
+++ clase_2/ejemplos/ex_1/app.js
@@ -0,0 +1,33 @@
+
+
+// Ejemplo arreglos con objetos
+// var arreglos = [
+//   {'name': 'Omar', 'age': 29, 'test': false},
+//   {'name': 'Omar', 'age': 29, 'test': false},
+//   {'name': 'Omar', 'age': 29, 'test': false},
+//   {'name': 'Omar', 'age': 29, 'test': false},
+//   {'name': 'Omar', 'age': 29, 'test': false}
+// ]
+
+// Como hacer debugueo dentro de nuestro código, abrir el inspect element
+// debugger
+
+// Iterar sobre nuestro arreglo
+// for (var i = 0; i < arreglos.length; i++) {
+//   console.log(arreglos[i]);
+// }
+
+// Confirm
+// if (confirm('Estas seguro que deseas borrar este elemento?')) {
+//   alert('borrado');
+// } else {
+//   console.log('continue');
+// }
+
+// Prompt: input
+// var val = prompt('Porfavor inserte un valor');
+// console.log(val);
+// alert(val);
+
+//  Informativo
+// alert('Hello world');
