5e00d5eb298369903cd921620d4ca8acd1b02f29
Omar Montalvo <xhamir.one@gmail.com>
2019-12-08 10:31:41 -0600

    Ejemplos

--- clase_3/ejemplos/app.js
+++ clase_3/ejemplos/app.js
@@ -1,11 +1,22 @@
-var listado = document.querySelectorAll('li:nth-child(odd)');
 
-console.log(listado);
 
-for (var i = 0; i < listado.length; i++) {
-  listado[i].style.padding = "5px";
-  listado[i].innerText = "ELemento Impar";
-}
+// var impar = document.querySelectorAll('li:nth-child(odd)');
+//
+// for (var i = 0; i < impar.length; i++) {
+//   // Concatenar usando BACKTICKS
+//   // variables van dentro de ${}
+//   // Para escribir backtick es ATLGR+}
+//   impar[i].innerHTML = `Elemento Impar <b>${i+1}</b>`
+// }
+//
+// var listado = document.querySelectorAll('li:nth-child(odd)');
+//
+// console.log(listado);
+//
+// for (var i = 0; i < listado.length; i++) {
+//   listado[i].style.padding = "5px";
+//   listado[i].innerText = "ELemento Impar";
+// }
 
 // obtener el primer elemento impar
 // document.querySelector('li:nth-child(odd)')
@@ -13,17 +24,17 @@ for (var i = 0; i < listado.length; i++) {
 // document.querySelector('li:nth-child(even)')
 
 
-document.querySelector('.container').style.padding = "20px";
-
-document.querySelector('#titulo').innerText = "Ejemplo";
-
-document.querySelector('.listado').style.color = "blue";
-
-document.querySelector('li:last-child').innerHTML = "<b>Test</b> Code";
-
-test = document.querySelector('li:nth-child(8)')
-
-console.log(test);
-
-test.innerText = "Prueba con una variable";
-test.style.color = 'red';
+// document.querySelector('.container').style.padding = "20px";
+//
+// document.querySelector('#titulo').innerText = "Ejemplo";
+//
+// document.querySelector('.listado').style.color = "blue";
+//
+// document.querySelector('li:last-child').innerHTML = "<b>Test</b> Code";
+//
+// test = document.querySelector('li:nth-child(8)')
+//
+// console.log(test);
+//
+// test.innerText = "Prueba con una variable";
+// test.style.color = 'red';
