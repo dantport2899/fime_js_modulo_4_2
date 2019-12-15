41281b8d1874c40a242606d8fb64d922ee8e6cfb
Omar Montalvo <xhamir.one@gmail.com>
2019-12-15 12:01:27 -0600

    Ejemplos

--- clase_4/ejemplos/app.js
+++ clase_4/ejemplos/app.js
@@ -1,3 +1,13 @@
+var botones = document.querySelectorAll('.btn-danger');
+
+for (var i = 0; i < botones.length; i++) {
+  botones[i].addEventListener('click', eliminarFila);
+}
+
+function eliminarFila(e) {
+  e.target.parentElement.remove();
+}
+
 // window.onload = "alCargar()";
 
 // function alCargar() {
